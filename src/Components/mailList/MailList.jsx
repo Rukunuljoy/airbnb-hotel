5a81dbbd0424e46c8a6412618d5f79a5350ab905
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-09-15 00:40:30 +0600

    maillist is complete

--- src/Components/mailList/MailList.jsx
+++ src/Components/mailList/MailList.jsx
@@ -1,10 +1,18 @@
-
+import "./MailList.css";
 const MailList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="mail">
+      <h1 className="mailTitles">save time,save money</h1>
+      <span className="mailDesc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        dolore!
+      </span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="your email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default MailList;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from '../../Components/featured/Featured';
 import FeaturedProperties from '../../Components/featuredProperties/FeaturedProperties';
 import Header from '../../Components/header/Header';
+import MailList from '../../Components/mailList/MailList';
 import Navbar from '../../Components/navbar/Navbar';
 import PropertyList from '../../Components/propertyList/PropertyList';
 import './Home.css'
@@ -16,6 +17,7 @@ const Home = () => {
     
       <h1 className='homeTitle'>Home guest love</h1>
       <FeaturedProperties/>
+      <MailList/>
     </div>
   </div>;
 };
