f907b7c6f291711a39d29300d0e0ca95f1d1cb78
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-09-15 02:10:56 +0600

    list start

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,11 +1,22 @@
 import Header from "../../Components/header/Header";
 import Navbar from "../../Components/navbar/Navbar";
+import './List.css'
 
 const List = () => {
   return <div>
     <Navbar/>
     <Header type="list"/>
-    
+    <div className="listContainer">
+      <div className="listWrapper">
+        <div className="listSearch">
+          <h1 className="listTitle">Search</h1>
+          <div className="lsItem">
+            <label htmlFor="/">Destination</label>
+          </div>
+        </div>
+        <div className="listResult"></div>
+      </div>
+    </div>
   </div>;
 };
 
